703f675ac25abba35bd8f38d9699741209a0028e
IVDen <stay_puft_marsh@list.ru>
2023-04-06 13:47:24 +0400

    Создали два метода заполнения и вывода массива на экран

--- KR/Program.cs
+++ KR/Program.cs
@@ -1,1 +1,42 @@
 ﻿
+string [] arr = new string[9]; ;
+
+// Заполняем массив с клавиатуры
+
+void PutInArr (string [] arr1) {
+
+    Console.WriteLine ("Заполните массив 10 строками символов");
+
+     for (int i = 0; i < arr1.Length; i++){
+
+     arr1[i] = Console.ReadLine();
+     
+     }
+     }
+
+// Отобразим полученные строки
+void ShowArr (string [] arr2) {
+ 
+ for (int i = 0; i < arr2.Length; i++){
+
+    Console.WriteLine (arr2[i]);
+
+}
+}
+
+//выводим строки из условия задачи (кол-во символов меньше или равно 3)
+
+void ShowStrings (string [] arr3) {
+
+     for (int i = 0; i < arr3.Length; i++){
+
+        if ()
+
+}
+
+
+
+PutInArr(arr);
+Console.ReadLine();
+Console.ReadLine();
+ShowArr(arr);
